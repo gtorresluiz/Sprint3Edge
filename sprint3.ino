9e12b9d30632984d37acb77bce3d39cfff62038b
Gabriel Torres <126830021+gtorresluiz@users.noreply.github.com>
2023-09-17 19:50:45 -0300

    Update sprint3.ino

--- sprint3.ino
+++ sprint3.ino
@@ -9,9 +9,9 @@ const char *serverAddress = "https://admin.tago.io/devices/650772ec19643c001064c
 const char *contentHeader = "application/json";
 const char *tokenHeader = "b7dd6c40-6b63-4743-8bb9-37708a72023e";
 const int sensorPin = 2;  // Pin do sensor de presença
-const int motorPin = 12;  // Pin do motor (altere para o seu setup)
+const int motorPin = 12;  // Pin do motor 
 
-bool isMotionDetected = false;
+bool presenca = false;
 
 void setup() {
   Serial.begin(9600);
@@ -22,9 +22,9 @@ void setup() {
 
 void loop() {
   // Verifique se o sensor de presença detectou movimento
-  isMotionDetected = digitalRead(sensorPin) == HIGH;
+  presenca = digitalRead(sensorPin) == HIGH;
 
-  if (isMotionDetected) {
+  if (presenca) {
     // Ligue o motor para abrir a tampa da privada (você pode precisar ajustar a lógica do motor)
     digitalWrite(motorPin, HIGH);
     delay(2000);  // Mantenha o motor ligado por 2 segundos (ajuste conforme necessário)
